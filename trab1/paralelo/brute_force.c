10de7e71e442334f208010759ff4992a28a1c15b
Rick <japa.eihara@gmail.com>
2015-09-10 20:56:24 -0300

    Algumas alterações

--- trab1/paralelo/brute_force.c
+++ trab1/paralelo/brute_force.c
@@ -13,6 +13,7 @@
 #define CONS_THREADS 3
 #define PROD_THREADS 1
 #define TAM_HASH 256
+#define TAM_VECTOR 3
 
 void CalculaHashSenha(const char *senha, char *hash);
 int IncrementaSenha(char *senha);
@@ -22,10 +23,21 @@ void start();
 void * consumePassword();
 void * createPassword();
 void foundPassword();
+void initializePassword();
+
+// Mutex para bloquear quando a senha esta sendo gerada
+pthread_mutex_t isTherePassword[TAM_VECTOR];
+
+// Mutex para bloequar acesso quando a senha está sendo utilizada
+// e não esta pronto para ser utilizada
+pthread_mutex_t canIUseThePassword[TAM_VECTOR];
 
 int tamanhoSenha = 0;
 
-char *senha;
+char ** senha;
+char * actualPassword;
+
+int passwordLength;
 
 int main(int argc, char *argv[])
 {
@@ -34,15 +46,16 @@ int main(int argc, char *argv[])
     return 1;
   }
 
+  // Define tamanho da senha passada como parâmetro
+  passwordLength = atoi(argv[1]);
+
   int i;
-  int tamanhoSenha;
 
   clock_t start = 0;
   clock_t finish = 0;
 
   long thread;
   pthread_t * thread_handles = malloc((CONS_THREADS+PROD_THREADS)*sizeof(pthread_t));
-
   start = clock();
 
   // Criação de threads de consumidores e produtores respectivamente
@@ -51,19 +64,24 @@ int main(int argc, char *argv[])
   for(i=0;i<PROD_THREADS;i++)
     pthread_create(&thread_handles[i+CONS_THREADS], NULL, createPassword, (void *) thread);
 
-  // Inicializa senha
-  tamanhoSenha = atoi(argv[1]);
-  senha = malloc((tamanhoSenha +1)*sizeof(char));
-  memset(senha, '0', tamanhoSenha);
-  senha[tamanhoSenha] = '\0';
+  // inicializando primeira senha a ser quebrada
+  actualPassword = malloc((passwordLength + 1)*sizeof(char));
+  memset(senha, '0', passwordLength);
+  actualPassword[passwordLength] = '\0';
 
   // Limpeza das threads
   free(thread_handles);
-
   finish = clock();
 }
 
+void * consumePassword(){
+  int i;
 
+  for(i=0; i < TAM_VECTOR; i++){
+
+  }
+
+}
 
 int TestaSenha(const char *hashAlvo, const char *senha) {
   int result = 0;
@@ -81,29 +99,24 @@ int TestaSenha(const char *hashAlvo, const char *senha) {
   return result;
 }
 
-int IncrementaSenha(char *senha)
-{
+void * createPassword(){
   int index = tamanhoSenha - 1;
 
-  while (index >= 0)
-  {
-    if (senha[index] != '9')
-    {
+  char * newPassword = (char*)malloc(sizeof(char)*passwordLength);
+
+
+  while (index >= 0){
+    if (senha[index] != '9'){
       senha[index]++;
       index = -2;
-    }
-    else
-    {
+    }else
       senha[index] = '0';
-    }
     index--;
   }
-  return index;
 }
 
 void CalculaHashSenha(const char *senha, char *hash) {
   struct crypt_data data;
   data.initialized = 0;
-
   strcpy(hash, crypt_r(senha, "aa", &data));
 }

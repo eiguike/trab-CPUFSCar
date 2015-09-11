08a21cbd8f337be1ffe2b81a77704326b30e0987
Rick <japa.eihara@gmail.com>
2015-09-11 02:56:51 -0300

    Perde um pouco para o código serializado

--- trab1/paralelo/labSO.c
+++ trab1/paralelo/labSO.c
@@ -16,23 +16,37 @@
 // Biblioteca que possui a função clock();
 #include <time.h>
 
-#define N_ITENS 100
+#define N_ITENS 2000
 //#define TAM_SENHA 4
 #define TAM_HASH 256
 #define NUM_PRODUTOR 1
-#define NUM_CONSUMIDOR 4
+#define NUM_CONSUMIDOR 2
 
 char * senhaAlvo;
 char ** buffer;
+char ** buffer2;
 char * senha;
 char * auxiliar;
+
+// variáveis para controle do tempo de duração da execução do programa
+clock_t start = 0;
+clock_t finish = 0;
 
 int TAM_SENHA;
 
 int inicio = 0, final = 0, cont = 0, encontrada = 0, finalizada = 0;
+int produtorNumeros = 0;
+int consumidorNumeros = 0;
 
-pthread_mutex_t bloqueio;
+pthread_mutex_t bloqueio, block, block2;
 pthread_cond_t nao_vazio, nao_cheio, encontrado;
+
+int incrementa_senha_conformeItens(){
+  int proxSenha = strtol(senha, NULL,10);
+  printf("Proxima senha: %d", proxSenha);
+
+  
+}
 
 int incrementa_senha() {
   int i;
@@ -66,27 +80,27 @@ int testa_senha(const char *hash_alvo, const char *senha) {
   if (!strcmp(hash_alvo, hash_calculado)) {
     return 0;
   }
+  //printf("%s %s %s\n",hash_alvo, hash_calculado, senha);
   return 1;
 }
 
 void * produtor(void *v) {
   char ** variavelLocal = NULL;
   int i;
-  int finalizado;
+  int finalizado = 1;
   do{
-    if(encontrada)
-      return NULL;
-
-    // bloqueia o buffer para que o produtor
-    // possa gerar mais senhas
-    pthread_mutex_lock(&bloqueio);
+    if(encontrada && !finalizada){
+      break;
+    }
     variavelLocal = (char**) malloc(sizeof(char*)*N_ITENS);
-
-    for(i=0;i<N_ITENS;i++)
+    for(i=0;i<N_ITENS;i++){
       variavelLocal[i] = NULL;
-
+    }
     i = 0;
     do{
+      if(!finalizado){
+        break;
+      }
       // local aonde se gera as chaves
       auxiliar = (char*)malloc(sizeof(char)*TAM_SENHA);
       strcpy(auxiliar, senha);
@@ -94,13 +108,25 @@ void * produtor(void *v) {
       i++;
     }while((i < N_ITENS)&&(finalizado = incrementa_senha()));
 
-    pthread_mutex_unlock(&bloqueio);
+     if(encontrada && !finalizado)
+       break;
+
+    pthread_mutex_lock(&bloqueio);
+    buffer = variavelLocal;
     pthread_cond_signal(&nao_vazio);
-
-    if(!finalizado)
-      return NULL;
-
+    pthread_cond_wait(&nao_cheio, &bloqueio);
+    if(encontrada && !finalizado)
+      break;
+    pthread_mutex_unlock(&bloqueio);
   }while(1);
+
+  produtorNumeros++;
+  while(consumidorNumeros < NUM_CONSUMIDOR){
+    pthread_cond_signal(&nao_vazio);
+    pthread_mutex_unlock(&bloqueio);
+  }
+
+  printf("Produtor morreu...\n");
 }
 
 void* consumidor(void *v) {
@@ -111,41 +137,47 @@ void* consumidor(void *v) {
   int i;
 
   do{
-    // quando já foi encontrada a chave, é finalizado
-    // thread
-    if(encontrada)
-      return NULL;
+    if(encontrada){
+      finalizada++;
+      break;
+    }
 
-    // bloqueia o acesso de outras threads
-    // no buffer
     pthread_mutex_lock(&bloqueio);
     variavelLocal2 = buffer;
     buffer = NULL;
-    cont = 0;
+
+    pthread_cond_signal(&nao_cheio);
+    pthread_cond_wait(&nao_vazio, &bloqueio);
     pthread_mutex_unlock(&bloqueio);
 
-    for(i=0; i < N_ITENS; i++)
+    for(i=0; (i < N_ITENS) && (variavelLocal2 != NULL); i++){
       if(variavelLocal2[i] != NULL)
         if(!testa_senha(senhaAlvo, variavelLocal2[i])){
           encontrada = 1;
-          return NULL;
+          finalizada++;
+          pthread_cond_signal(&nao_cheio);
         }else{
           free(variavelLocal2[i]);
         }
-    free(variavelLocal2);
+    }
 
-    // desbloqueia para que produtor crie mais
+    if(encontrada)
+      break;
+    free(variavelLocal2);
+  }while(1);
+
+  consumidorNumeros++;
+
+  while(produtorNumeros <  NUM_PRODUTOR){
     pthread_cond_signal(&nao_cheio);
     pthread_mutex_unlock(&bloqueio);
-  }while(1);
+  }
+
+  printf("Consumidor morreu...\n");
 }
 
 int main(int argc, char *argv[]) {
   int i;
-
-  // variáveis para controle do tempo de duração da execução do programa
-  clock_t start = 0;
-  clock_t finish = 0;
 
   // verifica o nÃºmero de argumentos
   if(argc != 3){
@@ -172,17 +204,19 @@ int main(int argc, char *argv[]) {
 
   // cria os estados e o mutex
   pthread_mutex_init(&bloqueio, NULL);
+  pthread_mutex_init(&block, NULL);
+  pthread_mutex_init(&block2, NULL);
   pthread_cond_init(&nao_cheio, NULL);
   pthread_cond_init(&nao_vazio, NULL);
   pthread_cond_init(&encontrado, NULL);
 
   // cria as threads
   start = clock();
-  for(i = 0; i < NUM_PRODUTOR; i++)
-    pthread_create(&thr_produtor[i], NULL, produtor, NULL);
   for(i = 0; i < NUM_CONSUMIDOR; i++)
     pthread_create(&thr_consumidor[i], NULL, consumidor, NULL);
 
+  for(i = 0; i < NUM_PRODUTOR; i++)
+    pthread_create(&thr_produtor[i], NULL, produtor, NULL);
   // finaliza as threads
   for(i=0; i < NUM_PRODUTOR; i++)
     pthread_join(thr_produtor[i], NULL);

b5768adcfeab0cfdcdbf3d20960cf062b44c68f1
rick <rick@rick.com>
2015-09-25 11:35:27 -0300

    Alteração feito em sala de aula

--- trab2/estrutura.h
+++ trab2/estrutura.h
@@ -11,14 +11,20 @@ fila * tail;
 fila * head;
 
 void adicionaFila(int * senhas){
+/*
 	fila aux = (fila*)malloc(sizeof(fila)*1);
 	aux->senhas = senhas;
 	aux = head;
 	aux->prox = NULL;
 	aux->prox = NULL;
-	if(tail == NULL){
-		tail->prox 	
+	if(head == NULL){
+		head = aux;
+		tail = aux;
+	}else{
+		tail->prox = aux;
+		tail = aux;
 	}
+*/
 }
 
 void criarFila(){
@@ -26,8 +32,8 @@ void criarFila(){
 	tail = NULL;
 }
 
-fila * removerFila(){
-
+int * removerFila(){
+	
 }
 
 
